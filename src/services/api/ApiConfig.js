a3f996d8a6420ad8df83433a1987838116988b2a
MatheusEzaquiel <matheusbezaquiel@gmail.com>
2024-03-20 18:36:38 -0300

    build: add link to localhost api

--- src/services/api/ApiConfig.js
+++ src/services/api/ApiConfig.js
@@ -5,7 +5,7 @@ export const Api = () => {
     if(!localStorage.getItem("authToken")) {
 
         return axios.create({
-            baseURL: "https://todolist-q55z.onrender.com"
+            baseURL: "http://localhost:8080",
         })
 
     } else {
@@ -13,7 +13,7 @@ export const Api = () => {
         let token = localStorage.getItem("authToken")
 
         return axios.create({
-            baseURL: "https://todolist-q55z.onrender.com",
+            baseURL: "http://localhost:8080",
             headers: {
                 "Authorization": `Bearer ${token}`
             }
